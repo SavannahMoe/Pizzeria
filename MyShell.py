a5acab0692f3c38c96db692d7130fd0dc78a4661
Savannah Moe <savannah_moe@baylor.edu>
2021-04-30 11:19:56 -0500

    creating home page

---
+++ MyShell.py
@@ -0,0 +1,26 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'Pizzeria.settings')
+
+import django
+django.setup()
+
+from pizza.models import Pizza, Topping
+
+pizzas = Pizza.objects.all()
+
+p = Pizza.objects.get(id=1)
+print(p)
+toppings = p.topping_set.all()
+for topping in toppings:
+    print(topping)
+
+'''
+for p in pizzas:
+    print(f"Pizza ID: {p.id} Pizza: {p}") 
+
+toppings = Topping.objects.all()
+
+for t in toppings:
+    print(f"Pizza: {t.pizza}")
+    print(f"Toppings: {t.name}")
+'''
